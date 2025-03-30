47274d4ed95880b82333f617a4814efafb07ab4e
agarwalkeshav185 <agarwalkeshav185@gmail.com>
2025-03-30 14:04:05 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const connect = require('./config/database');
 
-// const Tweet = require('./models/tweet');
-const TweetRepositoy = require('./repository/tweet-repository');
-const {PORT} = require('./config/serverConfig');
-const Comment = require('./models/comment');
 
+const {PORT} = require('./config/serverConfig');
 
 const app  = express();
 
@@ -13,11 +10,5 @@ app.listen(PORT, async ()=>{
     console.log(`Server started at port :- ${PORT}`);
     await connect();
     console.log('MongoDb Connected');
-    const repo = new TweetRepositoy();
-    const tweet = await repo.create({
-            content : 'with Hooks',
-            userEmail:'22112@iiitu.ac.in'   
-    });
-    // const tweet = await repo.getAll(0,1);
-    console.log(tweet);
+    
 });
